7637e1590e991884dd166964ddcff16d3733ae30
DS-kj <kunjangsherpa07@gmail.com>
2025-06-02 21:23:19 +0545

    dashboard dao database and model done

---
+++ src/Inventory/model/Product.java
@@ -0,0 +1,26 @@
+package Inventory.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private String category;
+    private int quantity;
+    private double price;
+    private double rate;
+
+    public Product(int id, String name, String category, int quantity, double price, double rate) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+        this.price = price;
+        this.rate = rate;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getCategory() { return category; }
+    public int getQuantity() { return quantity; }
+    public double getPrice() { return price; }
+    public double getRate() { return rate; }
+}
