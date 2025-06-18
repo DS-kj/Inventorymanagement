9cf6b87a232063b57ccce022fa4a16e33bcf2180
Nitess Katuwal <nitesskatuwal@Nitesss-MacBook-Pro.local>
2025-06-18 11:47:55 +0545

    All work

---
+++ src/InventoryManagementSystem/DAO/UserDao.java
@@ -0,0 +1,37 @@
+package InventoryManagementSystem.DAO;
+
+import InventoryManagementSystem.database.MySqlConnection;
+import InventoryManagementSystem.model.LoginRequest;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+    MySqlConnection mySql = new MySqlConnection();
+
+    public UserData login(LoginRequest loginRequest) {
+        String sql = "SELECT name, password FROM users WHERE name = ? AND password = ?";
+
+        try (Connection conn = mySql.openConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, loginRequest.getUsername());
+            stmt.setString(2, loginRequest.getPassword());
+
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    String email = rs.getString("name");
+                    String password = rs.getString("password");
+
+                    return new UserData(email, password);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
