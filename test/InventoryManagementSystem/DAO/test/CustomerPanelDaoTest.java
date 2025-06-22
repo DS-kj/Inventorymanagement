8a2975cdb8cc412eee2e6f33ed300403e86c80a3
Nitess Katuwal <nitesskatuwal@192.168.1.7>
2025-06-22 23:26:52 +0545

    getallcustomer

--- test/InventoryManagementSystem/DAO/test/CustomerPanelDaoTest.java
+++ test/InventoryManagementSystem/DAO/test/CustomerPanelDaoTest.java
@@ -1,37 +1,37 @@
-//package InventoryManagementSystem.dao.test;
-//
-//import InventoryManagementSystem.dao.CustomerPanelDao;
-//import InventoryManagementSystem.model.CustomerPanelModel;
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//import java.util.List;
-//
-//public class CustomerPanelDaoTest {
-//
-//    private final CustomerPanelDao dao = new CustomerPanelDao();
-//
-//    @Test
-//    public void testAddCustomer() {
-//        CustomerPanelModel customer = new CustomerPanelModel(
-//                0, "JUnit Test", "9999999999", "junit@example.com"
-//        );
-//
-//        boolean result = dao.addCustomer(customer);
-//        Assert.assertTrue("Customer should be added successfully", result);
-//    }
-//
-//    @Test
-//    public void testGetAllCustomers() {
-//        List<CustomerPanelModel> customers = dao.getAllCustomers();
-//        Assert.assertNotNull("Customer list should not be null", customers);
-//        for (CustomerPanelModel customer : customers) {
-//            Assert.assertNotNull("Customer name should not be null", customer.getName());
-//            Assert.assertNotNull("Customer mobile should not be null", customer.getMobileNumber());
-//            Assert.assertNotNull("Customer email should not be null", customer.getEmail());
-//        }
-//    }
-//
+package InventoryManagementSystem.dao.test;
+
+import InventoryManagementSystem.dao.CustomerPanelDao;
+import InventoryManagementSystem.model.CustomerPanelModel;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CustomerPanelDaoTest {
+
+    private final CustomerPanelDao dao = new CustomerPanelDao();
+
+    @Test
+    public void testAddCustomer() {
+        CustomerPanelModel customer = new CustomerPanelModel(
+                0, "JUnit Test", "9999999999", "junit@example.com"
+        );
+
+        boolean result = dao.addCustomer(customer);
+        Assert.assertTrue("Customer should be added successfully", result);
+    }
+
+    @Test
+    public void testGetAllCustomers() {
+        List<CustomerPanelModel> customers = dao.getAllCustomers();
+        Assert.assertNotNull("Customer list should not be null", customers);
+        for (CustomerPanelModel customer : customers) {
+            Assert.assertNotNull("Customer name should not be null", customer.getName());
+            Assert.assertNotNull("Customer mobile should not be null", customer.getMobileNumber());
+            Assert.assertNotNull("Customer email should not be null", customer.getEmail());
+        }
+    }
+
 //    @Test
 //    public void testDeleteCustomer() {
 //        // First, add a test customer to delete
