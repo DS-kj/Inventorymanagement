86daa48e1d936bfd0c78838e10fe43e9a8c3774e
Rijin Maharjan <mhzn.rijin8@gmail.com>
2025-06-22 14:08:13 +0545

    Made change in login page and also made sure it opens login when going back from logout

--- src/InventoryManagementSystem/controller/CustomerchooserController.java
+++ src/InventoryManagementSystem/controller/CustomerchooserController.java
@@ -6,6 +6,7 @@ import InventoryManagementSystem.view.Category;
 import InventoryManagementSystem.view.CustomerPanel;
 import InventoryManagementSystem.view.Customerchooser;
 import InventoryManagementSystem.view.Dashboard;
+import InventoryManagementSystem.view.LoginPanel;
 import InventoryManagementSystem.view.MainPage;
 import InventoryManagementSystem.view.ProductPanel;
 import InventoryManagementSystem.view.ProductandCart;
@@ -155,6 +156,9 @@ controllerCategory.open();
 
         if (response == javax.swing.JOptionPane.YES_OPTION) {
             view.dispose();
+            LoginPanel viewLogin=new LoginPanel();
+                LoginController LoginOpener= new LoginController(viewLogin);
+                 LoginOpener.open();
             System.out.println("User logged out.");
         } else {
             System.out.println("Logout cancelled.");
